e4721783aab144e382ee5c7baee98ac08a2be735
ib0uxx <farrattaque@gmail.com>
2025-03-05 10:36:59 +0100

    fix nom import et ajout d'imports

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 import { Board } from "./model/board.js";
 import { List } from "./model/list.js";
 import { Card } from "./model/card.js";
-import { BoardService } from "./app/BoardService.js";
-import { StorageService } from "./app/StorageService.js";
-import { DOMAdapter } from "./infra/DOMAdapter.js";
+import { BoardService } from "./app/boardService.js";
+import { StorageService } from "./app/storageService.js";
+import { DOMAdapter } from "./infrastructure/domAdapter.js";
 
 const storage = new StorageService();
 const boardService = new BoardService(storage);
@@ -13,9 +13,9 @@ const domAdapter = new DOMAdapter(boardService);
 domAdapter.renderBoard(board);
 
 document.getElementById("add-list").addEventListener("click", () => {
-    const listName = prompt("Nom de la liste :");
-    if (listName) {
-        boardService.addList(listName);
-        domAdapter.renderBoard(board);
-    }
+  const listName = prompt("Nom de la liste :");
+  if (listName) {
+    boardService.addList(listName);
+    domAdapter.renderBoard(board);
+  }
 });
